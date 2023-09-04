c1342d156b6c8a74e29981bc6c38151912fd00f0
Amacrel <alexandre.macrel@gmail.com>
2023-09-04 14:25:02 +0200

    [FEAT] add category entity

---
+++ src/main/java/amacrel/gescom_demo_spring/entities/Category.java
@@ -0,0 +1,37 @@
+package amacrel.gescom_demo_spring.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "category")
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category")
+    private int idCategory;
+    @Column(name = "name_category")
+
+    private String name;
+
+    public int getIdCategory() {
+        return idCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIdCategory(int idCategory) {
+        this.idCategory = idCategory;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
