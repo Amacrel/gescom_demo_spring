975f97528039e2a7504b445b095e00d23d74c906
Amacrel <alexandre.macrel@gmail.com>
2023-09-04 11:34:39 +0200

    first commit

---
+++ src/test/java/amacrel/gescom_demo_spring/GescomDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package amacrel.gescom_demo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GescomDemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
